d79dc78b2e4d945041793e4f846cafca91729444
tykoon787 <tykoon787@protonmail.com>
2023-02-12 13:17:07 +0300

    Updated User Class

--- models/user.py
+++ models/user.py
@@ -1,12 +1,23 @@
 from models.base_model import BaseModel
-from models import storage
 
 class User(BaseModel):
+    """
+    This is the user class
+    ...
+    Attributes
+    ----------
+    email: str
+        Email of the User
+    password: str
+        User Password
+    first_name: str
+        User First Name
+    last_name: str
+        User Last Name
+    """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-    def __init__(self):
-        super().__init__(email=self.email, password=self.password, first_name=self.first_name, last_name=self.last_name)
-
+    
 
