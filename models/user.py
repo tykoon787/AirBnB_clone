d19e96db0e842ef9c5fea15b9c2c26c42ea5d6b8
tykoon787 <tykoon787@protonmail.com>
2023-02-12 12:27:11 +0300

    Removed storage.new(self) method from the initializer

--- models/user.py
+++ models/user.py
@@ -8,6 +8,5 @@ class User(BaseModel):
     last_name = ""
     def __init__(self):
         super().__init__(email=self.email, password=self.password, first_name=self.first_name, last_name=self.last_name)
-        storage.new(self)
 
 
