82d5233e60a54d13a7d919ac49a314c9e2dd4c17
tykoon787 <tykoon787@protonmail.com>
2023-02-15 05:45:25 +0300

    these are test files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from models import storage
 from models.user import User
+from models.base_model import BaseModel
 
 if __name__ == "__main__":
 
@@ -11,6 +12,13 @@ if __name__ == "__main__":
     for obj_id in all_objs.keys():
         obj = all_objs[obj_id]
         print(obj)
+
+    print("---- Crete Base Model Clas --")
+    my_model = BaseModel()
+    my_model.name = "My_First_Model"
+    my_model.my_number = 89
+    my_model.save()
+    print(my_model)
 
     print("-- Create a new User --")
     my_user = User()
@@ -24,7 +32,7 @@ if __name__ == "__main__":
     print("-- Create a new User 2 --")
     my_user2 = User()
     my_user2.first_name = "John"
-    my_user2.email = "airbnb2@mail.com"
-    my_user2.password = "root"
+    my_user2.email = "panda@mail.com"
+    my_user2.password = "bamboo"
     my_user2.save()
     print(my_user2)
