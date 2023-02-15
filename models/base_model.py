d544c5a4ac0d559627b8a8f00f5ec56d1827d214
tykoon787 <tykoon787@protonmail.com>
2023-02-15 09:22:05 +0300

    Made an Edit to check for BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-from models import storage
+# from models import storage
 """
 This module contains the Base Class
 
@@ -38,19 +38,19 @@ class BaseModel():
             self.id = uuid4()
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-    def save(self):
-        """
-        Function taht updates the public instance
-        attribute `updated at` with the current date and time
-        """
-        storage.save()
-        self.updated_at = datetime.now()
+    # def save(self):
+    #     """
+    #     Function taht updates the public instance
+    #     attribute `updated at` with the current date and time
+    #     """
+    #     storage.save()
+    #     self.updated_at = datetime.now()
 
     def to_dict(self):
         """
