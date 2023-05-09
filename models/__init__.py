04a2a784f6423d8334e85a21625a9299f16f1593
tykoon787 <tykoon787@protonmail.com>
2023-05-09 21:47:35 +0300

    Updated Code

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from models.engine.file_storage import FileStorage
+import models.engine.file_storage as fs
 
-storage = FileStorage()
+storage = fs.FileStorage()
 storage.reload()
